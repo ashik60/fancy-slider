43c42a56620c01ac89188a621fb3af990af176cd
Ashik <ashikshaheen187@gmail.com>
2021-02-14 16:21:15 +0600

    Added Spinner

--- app.js
+++ app.js
@@ -26,9 +26,11 @@ const showImages = (images) => {
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div);
   });
+  toggleSpinner();
 };
 //pixabay.com/api/?key=15674931-a9d714b6e9d654524df198e00&q=nature&image_type=photo&pretty=true
 const getImages = (query) => {
+  toggleSpinner();
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then((response) => response.json())
     .then((data) => showImages(data.hits))
@@ -38,13 +40,13 @@ const getImages = (query) => {
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add("added");
+  element.classList.toggle("added");
 
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
   } else {
-    alert("Hey, Already added !");
+    sliders.splice(item, 1);
   }
 };
 var timer;
@@ -108,6 +110,11 @@ const changeSlide = (index) => {
 
   items[index].style.display = "block";
 };
+
+const toggleSpinner = () => {
+  const spinner = document.getElementById('loading-spinner');
+  spinner.classList.toggle('d-none');
+}
 
 searchBtn.addEventListener("click", function () {
   document.querySelector(".main").style.display = "none";
